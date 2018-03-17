d384484e328b1b63d87473be611c8c4dbc0b9c9a
Kody-J <36755234+Kody-J@users.noreply.github.com>
2018-03-16 21:19:48 -0500

    Updated Elevator.cpp to match python Mockup
    Added:
    1) Map for users, hasUSersRiding(), moveFloor(), userEnters(), and started generating main report

--- Elevator.h
+++ Elevator.h
@@ -1,19 +1,34 @@
-#pragma once
+#include <vector>
+#include <map>
 
-class Elevator {
+using namespace std;
+
+class Elevator : private User
+{
+
 private:
+
 	int height;
+	map<string, User> users;
 	int floor;
 	bool direction; // True = up
+
 public:
 	Elevator();
 	Elevator(int h, int f, bool d);
 
 	int getHeight();
 	int getFloor();
-	bool getDirection();
 
+	bool getDirrection();
+	bool hasUsersRiding();
+	
 	void setHeight(int newHeight);
 	void setFloor(int newFloor);
 	void setDirection(bool newDir);
+	void moveFloor();
+	void userEnters(User usr);
+	
+	~Elevator();
 };
+
