5248bce63a856b7b1392340d3954a0776909b04f
Kody-J <36755234+Kody-J@users.noreply.github.com>
2018-03-16 21:27:46 -0500

    Update Elevator.cpp to match python Mockup

--- Elevator.cpp
+++ Elevator.cpp
@@ -1,10 +1,12 @@
+#include "stdafx.h"
 #include "Elevator.h"
+
 
 const int DEFAULT_HEIGHT = 34;
 const int DEFAULT_FLOOR = 0;
 const int DEFAULT_DIRECTION = true; // heading up
 
-Elevator::Elevator() {
+Elevator::Elevator(){
 	height = DEFAULT_HEIGHT;
 	floor = DEFAULT_FLOOR;
 	direction = DEFAULT_DIRECTION;
@@ -14,11 +16,37 @@ Elevator::Elevator(int h, int f, bool d) {
 	floor = f;
 	direction = d;
 }
-
-int Elevator::getHeight() { return height; };
-int Elevator::getFloor() { return floor; };
-bool Elevator::getDirection() { return direction; };
-
-void Elevator::setHeight(int newHeight) { height = newHeight; };
-void Elevator::setFloor(int newFloor) { floor = newFloor; };
-void Elevator::setDirection(bool newDir) { direction = newDir; };
+int Elevator::getHeight(){return height;}
+int Elevator::getFloor(){return floor;}
+bool Elevator::getDirrection(){return direction;}
+bool Elevator::hasUsersRiding(){return users.size() == 0;}
+void Elevator::moveFloor(){
+	vector<string> usersToRemove;
+	int increment;
+	if (direction)
+		increment = 1;
+	else
+		increment = -1;
+	floor += increment;
+	for (map<string, User>::iterator itr = users.begin(); itr != users.end(); itr++)
+	{
+		itr->second.moveFloor();
+		itr->second.setCurrentFloor(floor);
+		if (itr->second.hasReachedDestination())
+		{
+			usersToRemove.push_back(itr->second.getUserId());
+		}
+	}
+	for (int i = 0; i < usersToRemove.size(); i++){
+		users[usersToRemove[i]].addBoardingDuration();
+		users.erase(usersToRemove[i]);
+	}
+}
+void Elevator::userEnters(User usr){
+	usr.addBoardingDuration();
+	users[usr.getUserId()] = usr;
+}
+void Elevator::setHeight(int newHeight){height = newHeight;}
+void Elevator::setFloor(int newFloor){floor = newFloor;}
+void Elevator::setDirection(bool newDir){direction = newDir;}
+Elevator::~Elevator(){}
